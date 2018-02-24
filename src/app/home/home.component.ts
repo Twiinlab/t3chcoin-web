5b17a66dd0320bf00717ad9a0af16fb123ff9731
ruben chavarri <ruben.chavarri.contractor@bbva.com>
2018-02-24 20:34:00 +0100

    udpate styles and home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,51 +8,9 @@ const PROFILE_PLACEHOLDER_IMAGE_URL = '/assets/images/profile_placeholder.png';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  socialMessages: any;
-  tableSocialColumns = ['photoUrl', 'socialId', 'totalAll', 'totalTwit', 'totalTwitRetweet', 'totalTwitLike'];
-
-  userMessages: any;
-  tableUserColumns = ['avatar', 'selectedItem', 'userName', 'itemsCount', 'balance'];
-
-  constructor(public t3chcoinService: T3chcoinService) { }
-
-  ngOnInit() {
-    this.initSocialMessage();
-    this.initUserMessage()
-  }
-
-  initSocialMessage() {
-    this.t3chcoinService
-    .getTopSocials()
-    .subscribe(socials => {
-      this.socialMessages = socials.map(item => {
-        return {
-          photoUrl: PROFILE_PLACEHOLDER_IMAGE_URL,
-          socialId: item.socialId || '#UserName',
-          totalAll: item.totalAll,
-          totalTwit: item.totalTwit,
-          totalTwitLike: item.totalTwit,
-          totalTwitRetweet: item.totalTwitRetweet
-        }
-      });
-    });
-  }
-
-  initUserMessage() {
-    this.t3chcoinService
-    .getUserTopList()
-    .subscribe(user => {
-      this.userMessages  = user.map(item => {
-        return {
-          avatar: PROFILE_PLACEHOLDER_IMAGE_URL,
-          selectedItem: item.selectedItem,
-          userName: item.userName || '#UserName',
-          balance: item.balance,
-          itemsCount: item.itemsCount
-        }
-      });
-    });
+  openLink(urlLink) {
+    window.open(urlLink, '_blank');
   }
 }
